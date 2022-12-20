468b4d5f6d4f6047cf4519497c723b4eb89e4086
abikena <requenaabigail2@gmail.com>
2022-12-20 00:38:36 -0600

    concatenacion e interpolacion p1

---
+++ java-script/inde.js
@@ -0,0 +1,8 @@
+let nombre =prompt ("Bienvenido, proporciona tu nombre"); 
+const apellido = prompt("Menciona tu primer apellido");
+
+//INTERPOLACION o TRANSPOLACION 
+const entrada = alert (`Bienvenido ${nombre} ${apellido}`)
+
+console.log (nombre);
+console.log (apellido);
